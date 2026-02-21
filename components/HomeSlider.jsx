cac1277dc34e3d5f54d2346018ff9e6e841647b1
rajdeotarse8007 <rajdeotarse8007@gmail.com>
2026-02-21 11:23:25 +0530

    add remaining

--- components/HomeSlider.jsx
+++ components/HomeSlider.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 const images = [
-  "https://scontent.fpnq7-9.fna.fbcdn.net/v/t39.30808-6/473438773_627238712992619_4032268673460601074_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=MzuoU-ZrD50Q7kNvwFfcYzB&_nc_oc=AdmRtITySVzsMp8i5ZX9WntDx_woKuTkwwUP_F6sZxkJlC29cKImDghbSM3B5zxwPezygpt2V9fNXukO61qNd_aF&_nc_zt=23&_nc_ht=scontent.fpnq7-9.fna&_nc_gid=qy28yD1-kuB1yYEMQDdZ_Q&oh=00_AftMMUDJCpR7g4eIiwT2EbIagI-eomZEXfUCdv1afWtDDw&oe=698A2E3E",
+  "https://scontent.fpnq13-5.fna.fbcdn.net/v/t39.30808-6/473438773_627238712992619_4032268673460601074_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=108&ccb=1-7&_nc_sid=2a1932&_nc_ohc=a4J6a5YvgB0Q7kNvwGN0MCD&_nc_oc=AdlZtIfeBP9rW3vScay1sZR-e4YzrYu4IteIChCtZtABLrYXsE_cmxzMPhAsGj7fG4KV3h3-xxQxncheAwzu72kT&_nc_zt=23&_nc_ht=scontent.fpnq13-5.fna&_nc_gid=hSsStOL-BC9HMXM5dNyo6A&oh=00_AfvU-anf6lCXQj1C_Ob-t7T-lh-pdJdwy4PHpBW5NaXF0w&oe=699F0DFE",
   "https://static.vecteezy.com/system/resources/thumbnails/066/206/039/small/delicate-saffron-strands-sweeping-across-aromatic-cumin-seed-landscape-photo.jpg",
   "https://kunshmasale.com/wp-content/uploads/2024/02/3-1920x964.webp",
   "https://kunshmasale.com/wp-content/uploads/2024/02/3-2-1920x964.webp",
